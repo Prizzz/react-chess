b4b83be4b65d6f2c586d5a039c759efb2f7e22a6
Prizzz <rob_243248@mail.ru>
2022-09-11 17:10:35 +0400

    feat: creat board

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import BoardComponent from './components/BoardComponent';
+import { Board } from './models/Board';
 
 function App() {
-  return <div className="App"></div>;
+  const [board, setBoard] = useState(new Board());
+
+  useEffect(() => {
+    restart();
+  }, []);
+
+  function restart() {
+    const newBoard = new Board();
+    newBoard.initCell();
+    setBoard(newBoard);
+  }
+
+  return (
+    <div className="app">
+      <BoardComponent board={board} setBoard={setBoard} />
+    </div>
+  );
 }
 
 export default App;
